252ed93450549106cda0cd43e400b155b6708ed1
SergDS <slovotanyserga@gmail.com>
2018-12-28 12:46:38 +0300

    Controls

--- launcher.py
+++ launcher.py
@@ -1,3 +1,9 @@
 import engine
-
+print "W: forward"
+print "S: back"
+print "A: strafe left"
+print "D: strafe right"
+print "Mouse: look around"
+print "Space: jump"
+print "Tab: toggle flying mode"
 engine.main()
